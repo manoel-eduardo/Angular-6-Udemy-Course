9aa09c8ae18f0aaa097597c856b8eee1b3d97d6d
Eduardo Manoel <edu.manoel.p@gmail.com>
2018-07-05 19:17:19 -0300

    Assignment two

---
+++ assignment-two/src/app/username-input/username-input.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username-input',
+  templateUrl: './username-input.component.html',
+  styleUrls: ['./username-input.component.css']
+})
+export class UsernameInputComponent implements OnInit {
+
+  username = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  reset(){
+    this.username = '';
+  }
+}
